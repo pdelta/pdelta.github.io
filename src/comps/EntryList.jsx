e8b86f06374f33b20f1242e61b83496b3e2298bb
moodysalem <a454465c8beef70811a01489042c79b7f235df5e>
2017-05-04 19:07:05 -0500

    group the entry list

--- src/comps/EntryList.jsx
+++ src/comps/EntryList.jsx
@@ -2,6 +2,8 @@ import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import _ from 'underscore';
 
+const firstChar = str => str.trim()[ 0 ];
+
 export default class EntryList extends Component {
   static propTypes = {
     entries: PropTypes.arrayOf(PropTypes.string).isRequired,
@@ -11,22 +13,36 @@ export default class EntryList extends Component {
   render() {
     const { entries, onSelectEntry } = this.props;
 
+    const groupedEntries = _.groupBy(entries, firstChar);
+
     return (
-      <ul className="nav nav-pills">
+      <div>
         {
           _.map(
-            entries,
-            (store) => (
-              <li key={store}>
-                <a href="#" onClick={e => {
-                  e.preventDefault();
-                  onSelectEntry(store);
-                }}>{store}</a>
-              </li>
+            groupedEntries,
+            (group, firstChar) => (
+              <div key={firstChar}>
+                <h2>{firstChar}</h2>
+                <ul className="nav nav-pills">
+                  {
+                    _.map(
+                      group,
+                      store => (
+                        <li key={store}>
+                          <a href="#" onClick={e => {
+                            e.preventDefault();
+                            onSelectEntry(store);
+                          }}>{store}</a>
+                        </li>
+                      )
+                    )
+                  }
+                </ul>
+              </div>
             )
           )
         }
-      </ul>
+      </div>
     );
   }
 }
