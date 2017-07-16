bb4baee8fddcd669f07199b99d98b8b6907b5438
moodysalem <a454465c8beef70811a01489042c79b7f235df5e>
2017-07-16 13:02:35 -0400

    github fork ribbon

--- src/comps/AppShell.jsx
+++ src/comps/AppShell.jsx
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import Nav from './Nav';
 import NSP from './NSP';
+import GitHubForkRibbon from 'react-github-fork-ribbon';
 
 export default class AppShell extends Component {
   render() {
@@ -8,6 +9,9 @@ export default class AppShell extends Component {
 
     return (
       <NSP>
+        <GitHubForkRibbon target="_blank" position="left-bottom" href="https://github.com/pdelta/pdelta.github.io">
+          Fork me on GitHub
+        </GitHubForkRibbon>
         <div>
           <Nav/>
           <div>
